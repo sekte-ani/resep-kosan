990f40f2f1d1a8982e154b735c2969b4bc532c3c
str22ui <satria.ramadhn48@gmail.com>
2024-01-12 18:13:30 +0700

    add pagination

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -31,7 +31,7 @@ class MenuController extends Controller
 
     public function minuman()
     {
-        $minuman = Menu::with('category')->where('category_id', 'name', 'minuman')->paginate(6);
+        $minuman = Menu::with('category')->where('category_id', '=', '2')->paginate(6);
         $menu = Menu::latest();
 
         if (request('search')) {
@@ -45,7 +45,7 @@ class MenuController extends Controller
 
     public function cemilan()
     {
-        $cemilan = Menu::with('category')->where('category_id', 'name', 'cemilan')->paginate(6);
+        $cemilan = Menu::with('category')->where('category_id', '=', '3')->paginate(6);
         $menu = Menu::latest();
 
         if (request('search')) {
