7666dae2935b3d555857ac1c60aad8c7aa225a62
str22ui <satria.ramadhn48@gmail.com>
2024-01-12 18:22:40 +0700

    add pagination

--- resources/views/cemilan/detail.blade.php
+++ resources/views/cemilan/detail.blade.php
@@ -73,7 +73,7 @@
 
     </div>
 
-    <div id="modal" class="fixed top-0 left-0 w-full h-full flex items-center justify-center hidden">
+    <div id="modal" class="fixed top-0 left-0 w-full h-full  items-center justify-center hidden">
         <div class="modal-overlay absolute w-full h-full bg-gray-900 opacity-50"></div>
 
         <div class="modal-container bg-white w-11/12 md:max-w-md mx-auto rounded shadow-lg z-50 overflow-y-auto">
